c959cb61dec73ebf5e5a462e17de9259026e6c66
ThiagoDambroski <dambroskic@hotmail.com>
2022-09-06 20:57:54 -0300

    JPA Repository

--- src/main/java/com/dambroski/demo/student/StudentService.java
+++ src/main/java/com/dambroski/demo/student/StudentService.java
@@ -3,13 +3,25 @@ package com.dambroski.demo.student;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 	
+	
+	private final StudentRepository studentRepository;
+	
+	
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+
+
 	public List<Student> getStudents() {
-		return List.of(new Student(1L,"leticia","leticia@gmail.com",LocalDate.of(2002, 5, 23),20));
+		return studentRepository.findAll();
 	}
 	
 }
