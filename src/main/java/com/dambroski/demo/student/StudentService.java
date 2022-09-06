8ba87d6cb322cf7d983d93487b3ac5abf4704da0
ThiagoDambroski <dambroskic@hotmail.com>
2022-09-05 23:24:18 -0300

    Dependency injection

--- src/main/java/com/dambroski/demo/student/StudentService.java
+++ src/main/java/com/dambroski/demo/student/StudentService.java
@@ -3,6 +3,10 @@ package com.dambroski.demo.student;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
 	
 	public List<Student> getStudents() {
