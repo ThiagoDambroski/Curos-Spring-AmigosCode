c842acef0e4a10b07bd8ecd06681cd48582f9474
ThiagoDambroski <dambroskic@hotmail.com>
2022-09-07 21:36:35 -0300

    Post Mapping

--- src/main/java/com/dambroski/demo/student/StudentService.java
+++ src/main/java/com/dambroski/demo/student/StudentService.java
@@ -22,5 +22,12 @@ public class StudentService {
 	public List<Student> getStudents() {
 		return studentRepository.findAll();
 	}
+
+
+
+	public void addNewStudent(Student student) {
+		System.out.println(student);
+		
+	}
 	
 }
